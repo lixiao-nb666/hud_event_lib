5867ad7c76dca376c9882eb32cafb4bc084c1bfc
lixiao <lixiao@nb-666.com>
2025-07-11 17:34:14 +0800

    ..

---
+++ app/src/main/java/com/nrmyw/hud_data_event/app/MyApp.java
@@ -0,0 +1,16 @@
+package com.nrmyw.hud_data_event.app;
+
+import android.app.Application;
+
+
+
+
+public class MyApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+
+    }
+}
