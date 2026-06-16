994e09601ee688638547761be4b54c3a317fe61c
Li Xiao <lixiao@nb-666.com>
2026-06-16 21:51:45 +0800

    看下日志

--- hud_data_event_lib/src/main/java/com/nrmyw/hud_data_event_lib/util/HudShowStringUtil.java
+++ hud_data_event_lib/src/main/java/com/nrmyw/hud_data_event_lib/util/HudShowStringUtil.java
@@ -1,6 +1,7 @@
 package com.nrmyw.hud_data_event_lib.util;
 
 import android.text.TextUtils;
+import android.util.Log;
 
 import com.nrmyw.hud_data_event_lib.config.HudSetConfig;
 
@@ -10,11 +11,18 @@ public class HudShowStringUtil {
         if(TextUtils.isEmpty(checkStr)){
             return "";
         }
+        Log.i("kankangaibianchangewei","kankangaibianchangewei0");
         if(checkStr.length()>minL&&checkStr.length()<maxL){
-
+            if(checkStr.length()<12){
+                Log.i("kankangaibianchangewei","kankangaibianchangewei1");
+                return checkStr+"             ";
+            }else {
+                Log.i("kankangaibianchangewei","kankangaibianchangewei2");
+                return checkStr+"         ";
+            }
 //            int  needAddNumb=HudSetConfig.getInstance().getHudSetBean().getShowMaxNumb()-checkStr.length();
 //            return addEmp(checkStr,needAddNumb);
-            return checkStr+"         ";
+
         }
         return checkStr;
     }
