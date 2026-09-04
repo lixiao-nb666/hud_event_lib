d430e3d603337ac076ff9ec3e5b3a33f6b866f1d
Li Xiao <lixiao@nb-666.com>
2026-09-04 20:42:45 +0800

    修改了

--- hud_data_event_lib/src/main/java/com/nrmyw/hud_data_event_lib/manager/HudSendManager.java
+++ hud_data_event_lib/src/main/java/com/nrmyw/hud_data_event_lib/manager/HudSendManager.java
@@ -184,6 +184,7 @@ public class HudSendManager {
             case OPEN_OR_CLOSE_HOTPORT:
             case OPEN_OR_CLOSE_TRANSLATION_UI:
             case WARNING_POINT_PASSING:
+            case TAKE_PHOTO:
                 body = HudCmdSendDataUtil.getStatu(objects);
                 break;
             case SET_UI:
